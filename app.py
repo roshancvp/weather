5103793adc5029eb7955b07af3daeb4e0ac8e054
Roshan Prabhakar <roshancvp@gmail.com>
2016-07-08 10:06:15 -0700

    Added Second Page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def weather():
+    city = request.form['city']
+    return render_template('weather.html', city = city)
 
 if __name__ == "__main__":
     app.run(debug = True)
