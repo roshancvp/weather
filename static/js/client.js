eb2765a3e49e709b8c592cd070d6db227d2867ff
Roshan Prabhakar <roshancvp@gmail.com>
2016-07-08 12:21:35 -0700

    Two Page Functionality

--- static/js/client.js
+++ static/js/client.js
@@ -4,34 +4,31 @@ $(document).ready(function() {
     var city;
     var country;
 
+    console.log(city)
+
     $('#go').click(function() {
-        city = $('#city').val();
-        //country = $('#country').val();
-
-        // making the request
-        var url = "http://api.openweathermap.org/data/2.5/weather?q="
-        var key = "2c03335f9552691480c68fd9bc03d907";
-
-        var request = new XMLHttpRequest();
-        request.open("GET",
-                     url +
-                     city +
-                     //"," + country +
-                     "&appid=" +
-                     key, false);
-        request.send();
-
-        // parsing json data
-        var json = JSON.parse(request.responseText);
-
-        // Writing data
-        var temp = (parseFloat(json.main.temp) - 273.15).toFixed(1);
-
-        $('#temp').text(temp + "°C");
-        $('#desc').text(json.weather[0].description);
-
-        //$('#desc').text(json.weather[0].icon);
-
+      city = $('#city').val();
     });
 
+    if ($('#city').val() != undefined) {
+      var city = $('#city').val()
+      console.log(city)
+      var url = "http://api.openweathermap.org/data/2.5/weather?q="
+      var key = "2c03335f9552691480c68fd9bc03d907";
+      var request = new XMLHttpRequest();
+      request.open("GET",url + city + "&appid=" + key, false);
+      request.send();
+
+      // parsing json data
+      var json = JSON.parse(request.responseText);
+
+      // Writing data
+      var temp = (parseFloat(json.main.temp) - 273.15).toFixed(1);
+
+      $('#temp').text(temp + "°C");
+      $('#desc').text(json.weather[0].description);
+
+    }
+
+    //TODO:$('#desc').text(json.weather[0].icon);
 });
